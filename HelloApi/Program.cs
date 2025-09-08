f5d97a70b1841408f14feefd4231e49cb98a0601
n.kekes@moralius.com <n.kekes@moralius.com>
2025-09-08 11:53:38 +0300

    feat: add healthz endpoint

--- HelloApi/Program.cs
+++ HelloApi/Program.cs
@@ -1,6 +1,9 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/healthz", () => new { status = "ok" });
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
